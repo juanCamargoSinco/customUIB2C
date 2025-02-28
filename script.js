a76839d88285744891eb5d3d5ec9f1afaa289dd6
juanCamargoSinco <61333020+juanCamargoSinco@users.noreply.github.com>
2025-02-28 15:19:10 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("La página personalizada se ha cargado correctamente.");
+  // Puedes agregar más funciones o validaciones según lo requieras.
+});
